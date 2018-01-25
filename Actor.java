bcc23e53cd4987f6965c9af6d2373c84ef91375a
Lemon <s.lemon.2016@mymail.jcsu.edu>
2018-01-25 13:14:37 -0500

    first

---
+++ Actor.java
@@ -0,0 +1,31 @@
+
+public class Actor {
+String firstname;
+String lastname;
+	public Actor(String First, String Last) {
+		// TODO Auto-generated constructor stub
+		
+		firstname=First;
+		lastname=Last;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Actor A= new Actor("Arnold ","Schwarzenegger");
+		
+		System.out.println(A.firstname + A.lastname );
+		
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
